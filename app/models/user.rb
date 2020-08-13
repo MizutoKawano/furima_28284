3de6f99e3cabdb72b7932c96349f1198d741eb29
MizutoKawano <s54a068q@gmail.com>
2020-08-13 18:07:09 +0900

    バリデーションをまとめる、変数を使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+     with_options presence: true do
 
-  validates :family_name,                    presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は漢字で入力して下さい。' }
-  validates :last_name,                      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は漢字で入力して下さい。' }
-  validates :kana_family_name,               presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全カタカナで入力して下さい。' }
-  validates :kana_last_name,                 presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全カタカナで入力して下さい。' }
-  validates :nickname,                       presence: true
-  validates :email,                          presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は＠で入力して下さい。' }
-  validates :password,                       length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字で入力して下さい。' }
-  validates :birthday,                       presence: true
-end
+      亜あア=/\A[ぁ-んァ-ン一-龥]+\z/
+      カナ=/\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
+
+     validates :family_name,         format: { with: /#{亜あア}/, message: 'は漢字で入力して下さい。' }
+     validates :last_name,           format: { with: /#{亜あア}/, message: 'は漢字で入力して下さい。' }
+     validates :kana_family_name,    format: { with: /#{カナ}/, message: 'は全カタカナで入力して下さい。' }
+     validates :kana_last_name,      format: { with:/#{カナ}/, message: 'は全カタカナで入力して下さい。' }
+     validates :nickname
+     validates :email,               uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は＠で入力して下さい。' }
+     validates :birthday
+   end
+     validates :password,            length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字で入力して下さい。' }
+ end
+ 
