a1398b56d22cad52c8d6012a02efde03b42b3175
MizutoKawano <s54a068q@gmail.com>
2020-08-24 11:42:41 +0900

    before_actionでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
    before_action :move_to_index, except: [:index, :show]
+   before_action :set_edit, only: [:show, :edit, :update]
   def index
    @items = Item.all.order("created_at DESC")  
 end
 
 def show
- @item = Item.find(params[:id])
 end
 
  def new
@@ -28,16 +28,19 @@ end
  end
 
  def edit
-  @item =Item.find(params[:id])
  end
 
  def update
-  @item =Item.find(params[:id])
   if @item.update(item_params)
     redirect_to root_path
     else
-      render :edit
+    render :edit
   end
+ end
+
+
+ def set_edit
+  @item =Item.find(params[:id])
  end
 
 private
