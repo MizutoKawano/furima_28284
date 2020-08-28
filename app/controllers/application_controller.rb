78dcbae7e4bc011c67bca9e3395611a532aa54c8
MizutoKawano <s54a068q@gmail.com>
2020-08-28 17:22:44 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :last_name, :kana_family_name, :kana_last_name, :nickname, :birthday])
   end
 
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
